1500c4e51dc2bc81909a8408884858bb90682db2
tomasssantos484 <tomassantos484@gmail.com>
2024-02-23 13:30:19 -0500

    Completed all CUS1156_Lab 2 Tasks

--- src/UniqueWords.java
+++ src/UniqueWords.java
@@ -1,37 +1,57 @@
+/* Project: Lab 2, Git
+ * Class: UniqueWords
+ * Author: Tomas Santos Yciano
+ * Date: 02/19/2024
+ * 
+ * In this project, I have completed the countUnique method in order to properly count the amount of unique
+ * words in a list.
+ */
 import java.util.ArrayList;
 
-public class UniqueWords
-{
-   /**
-		counts the number of unique strings in a list
-		@param list ArrayList of strings to be examined
-		@return number of unique strings in the list
-   */
-   public static int countUnique(ArrayList<String> list)
-   {
-	  int count = 0;
-	  
-      for (int i = 0; i < list.size(); i++)
-      {		 for (int j = 0; j < list.size(); j++)
-		 {
-			
-		 }
-      }
-	  return count;
-   }
+public class UniqueWords {
+	/**
+	 * counts the number of unique strings in a list
+	 * 
+	 * @param list ArrayList of strings to be examined
+	 * @return number of unique strings in the list
+	 */
+	public static int countUnique(ArrayList<String> list) {
+		// Set up
+		int count = 0;
 
-   public static void main(String[] args)
-   {
-      ArrayList <String> words = new ArrayList<>();
-      words.add("apple");
-	  words.add("orange");
-	  words.add("blackboard");
-	  words.add("apple");
-	  words.add("orange");
-	  words.add("sun");
-	  words.add("moon");
-   
-	  int unique = countUnique(words);
-      System.out.println(words + " has " + unique + " unique words");
-   }
+		// Nested for loop
+		for (int i = 0; i < list.size(); i++) {
+			boolean isWordUnique = true; // Setting this to true for each iteration and word
+			for (int j = 0; j < list.size(); j++) {
+				// Check to see if word at position i is equal to any other word in list.
+				if (i != j && list.get(i).equals(list.get(j))) {
+					isWordUnique = false; // Word is NOT unique!
+					break;
+				}
+			}
+			// If the words ARE unique, increment the counter
+			if (isWordUnique) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public static void main(String[] args) {
+		// Testing in main method, added the words "football" and "basketball"
+		ArrayList<String> words = new ArrayList<>();
+		words.add("apple");
+		words.add("orange");
+		words.add("blackboard");
+		words.add("apple");
+		words.add("orange");
+		words.add("sun");
+		words.add("moon");
+		words.add("football");
+		words.add("basketball");
+
+		int unique = countUnique(words);
+		// With the current list of words, returns that there are 5 unique words in the list.
+		System.out.println(words + " has " + unique + " unique words");
+	}
 }
